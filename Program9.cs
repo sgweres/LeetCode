d6eed884fd21396c1c2cd5e10183113a67cf9e20
sgweres <sgweres1025@gmail.com>
2024-10-20 14:05:30 -0400

    Quadrant
    Takes an x and y axis point as input from the console and determines what quadrant the point is on a graph.

---
+++ Program9.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace assign3._1._4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int xCoord;
+            int yCoord;
+
+            Console.Write("Input the value for the x coordinate: ");
+            xCoord = int.Parse(Console.ReadLine());
+            Console.Write("Input the value for the y coordinate: ");
+            yCoord = int.Parse(Console.ReadLine());
+
+            InWhichQuadrant(xCoord,yCoord);
+
+        }
+        static void InWhichQuadrant(int x, int y)
+        {
+
+            if (x >= 0 && y >= 0)
+            { Console.WriteLine("The coordinate point " + x + "," + y + " lies in the first quadrant."); }
+            else if (x >= 0 && y < 0)
+            { Console.WriteLine("The coordinate point" + x + "," + y + " lies in the second quadrant."); }
+            else if (x < 0 && y < 0)
+            { Console.WriteLine("The coordinate point" + x + "," + y + " lies in the third quadrant."); }
+            else
+            { Console.WriteLine("The coordinate point" + x + "," + y + " lies in the fourth quadrant."); }
+        }   
+    }
+}
