9d6bf1c641d8f14917056a1d40a871a1f2d7a02b
sgweres <sgweres1025@gmail.com>
2024-10-20 09:19:45 -0400

    Reverse Array
    Using an integrated method to reverse the hard coded items in an array.

--- Program.cs
+++ Program.cs
@@ -4,82 +4,39 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Assignment_1._3._1
+namespace Assignment_1._3._2
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("What shape do you want to know the area for? Square, Rectangle, or Triangle?");
-            string inputShape;
+            int[] intArray = new int[3];
+            fillArray(intArray);
+            Console.WriteLine("The values stored in the array are: ");
+            displayArray(intArray);
+            Console.WriteLine("The values stored in the array in reverse are: ");
+            orderArray(intArray);
+            displayArray(intArray);
 
-            inputShape = Console.ReadLine();
 
-            ifOperation(inputShape);
-
-
-
+            
         }
-        static void areaSquare(double sideLength)
+        static void fillArray(int[] intArray)
         {
-            double areaReturn;
-            areaReturn = sideLength * sideLength;
-            Console.WriteLine("The area of the square is " + areaReturn);
+            intArray[0] = 2;
+            intArray[1] = 5;
+            intArray[2] = 7;
         }
-        static void areaRectangle(double width, double height)
+        static void orderArray(int[] intArray)
         {
-            double areaReturn;
-            areaReturn = width * height;
-            Console.WriteLine("The area of the rectangle is " + areaReturn);
-
+            Array.Reverse(intArray);
         }
-        static void areaTriangle(double baseLength, double height)
+        static void displayArray(int[] intArray)
         {
-            double areaReturn;
-            double halfNum;
-            halfNum = 0.5;
-            areaReturn = (halfNum * baseLength * height);
-            Console.WriteLine("The area of the triangle is " + areaReturn);
-        }
-        static void ifOperation(string inputShape)
-        {
-            if (inputShape == "Square" || inputShape == "square")
+            foreach (int item in intArray)
             {
-                double squareSide;
-
-                Console.WriteLine("Enter the length of a side of the square.");
-                squareSide = double.Parse(Console.ReadLine());
-                areaSquare(squareSide);
-            }
-            else if (inputShape == "Rectangle" || inputShape == "rectangle")
-            {
-                double rectangleWidth;
-                double rectangleHeight;
-
-                Console.WriteLine("What is the width of the rectangle?");
-                rectangleWidth = double.Parse(Console.ReadLine());
-                Console.WriteLine("What is the height of the rectangle?");
-                rectangleHeight = double.Parse(Console.ReadLine());
-                areaRectangle(rectangleWidth, rectangleHeight);
-
-            }
-            else if (inputShape == "Triangle" || inputShape == "triangle")
-            {
-                double triangleBase;
-                double triangleHeight;
-
-                Console.WriteLine("What is the base of the triangle?");
-                triangleBase = double.Parse(Console.ReadLine());
-                Console.WriteLine("What is the height of the triangle?");
-                triangleHeight = double.Parse(Console.ReadLine());
-                areaTriangle(triangleBase, triangleHeight);
-            }
-            else
-            {
-                Console.WriteLine("That is not a shape I know!");
+                Console.WriteLine(item);
             }
         }
-
-
     }
 }
